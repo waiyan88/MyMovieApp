379d6caf535c9b6ead1e9deaba58155b09f48511
waiyan88 <romeohom92@gmail.com>
2019-12-16 10:39:51 +0630

    Insert Finished

--- app/src/main/java/wy/chris/mymovieapp/MovieNavFragment.java
+++ app/src/main/java/wy/chris/mymovieapp/MovieNavFragment.java
@@ -1,6 +1,7 @@
 package wy.chris.mymovieapp;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 /**
@@ -25,7 +28,16 @@ public class MovieNavFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_nav, container, false);
+        View view= inflater.inflate(R.layout.fragment_movie_nav, container, false);
+        FloatingActionButton floatingActionButton=view.findViewById(R.id.addmovie);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getContext(),AddMovieActivity.class);
+                startActivity(intent);
+            }
+        });
+        return  view;
     }
 
 }
